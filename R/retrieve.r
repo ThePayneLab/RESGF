8b801bb892715f9522f03f258b401edfef50cfeb
Mark Payne <marpay@d8wrvp2.usr.local>
2021-05-26 10:44:26 +0200

    Option to retain retrieval log files in output

--- R/retrieve.r
+++ R/retrieve.r
@@ -15,6 +15,7 @@
 #' @param processes Number of processes to run in parallel
 #' @param skip.checksum Stops the wget script from verifying the checksum of the downloaded file. Corresponds to
 #' activating the "-p" parameter on the wget script.
+#' @param retain.logs Retain download logs in output
 #'
 #' @details When a resgf_status object is supplied, only files that are not held locally (but are listed in the manifest) are retrieved. For a manifest object, all files are retrieved
 #' are
@@ -23,7 +24,8 @@ resgf_retrieve <-
   function(object,
            node="http://esgf-node.llnl.gov/esg-search",
            processes=1,
-           skip.checksum=FALSE) {
+           skip.checksum=FALSE,
+           retain.logs=TRUE) {
 
     #Get list to retrieve
     if (attr(object,"checksums.verified")) { #Get missing and checksum failures (if any)
@@ -62,10 +64,13 @@ resgf_retrieve <-
       be.quiet <- processes > 1
       bash.cmd <- sprintf("cd %s && bash %s -s",local.dir,wget.fname)
       if(skip.checksum) bash.cmd <- sprintf("%s -p",bash.cmd)
-      get.this$retrieval.log <-
+      retrieval.log <-
         system(bash.cmd,
                intern=TRUE,
                ignore.stderr = be.quiet,ignore.stdout = be.quiet)
+      if(retain.logs) {
+        get.this$retrieval.log <- list(retrieval.log)
+      }
 
       #Assuming successful completion (how to check?) delete the script
       file.remove(wget.fname)
